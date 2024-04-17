8066fc22caaa9f5804783af3f6d4c42d47d6e8fa
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-04-17 09:42:40 +0200

    services und model dateien

---
+++ src/app/firebase.service/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnDestroy, inject } from '@angular/core';
+// import { Firestore, collection, onSnapshot, DocumentData, addDoc, doc, updateDoc, deleteDoc, getDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+// import { getFirestore } from "firebase/firestore";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserService implements OnDestroy {
+
+
+    ngOnDestroy(): void {
+        // throw new Error('Method not implemented.');
+    }
+
+
+
+
+}
