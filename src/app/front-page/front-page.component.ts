4a5c4d96c31791e6dad9511d48e6980c9bce0efb
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-04-17 12:29:50 +0200

    header test

--- src/app/front-page/front-page.component.ts
+++ src/app/front-page/front-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
 
 @Component({
   selector: 'app-front-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './front-page.component.html',
   styleUrl: './front-page.component.scss'
 })
