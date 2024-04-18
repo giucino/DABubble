2e40367bc7343da8fa92f3a5c7bdd6cbeb7dfe43
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-04-18 10:20:54 +0200

    login progress

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { SendEmailComponent } from './send-email/send-email.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, LoginComponent, SignInComponent, ResetPasswordComponent, AvatarComponent, SendEmailComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['/login']);
+  }
 }
