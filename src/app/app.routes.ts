397174868c3d6c2920434e1aa6b3f291b7f0e29d
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-04-18 09:54:21 +0200

    basic-layout & routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '' , component: LoginPageComponent},
+    { path: 'main-page' , component: MainPageComponent}
+];
