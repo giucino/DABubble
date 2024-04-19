84df853d801be1ca4b411743cc113a3dcae7ddf9
giucino <125276491+giucino@users.noreply.github.com>
2024-04-19 15:43:51 +0200

    Update main menu styles and components

--- src/app/main-page/main-menu/main-menu.component.ts
+++ src/app/main-page/main-menu/main-menu.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MainMenuHeaderComponent } from './main-menu-header/main-menu-header.component';
+import { MainMenuChannelsComponent } from './main-menu-channels/main-menu-channels.component';
+import { MainMenuDmComponent } from './main-menu-dm/main-menu-dm.component';
 
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MainMenuHeaderComponent,
+    MainMenuChannelsComponent,
+    MainMenuDmComponent,
+  ],
   templateUrl: './main-menu.component.html',
-  styleUrl: './main-menu.component.scss'
+  styleUrl: './main-menu.component.scss',
 })
-export class MainMenuComponent {
-
-}
+export class MainMenuComponent {}
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChannelComponent } from './channel/channel.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [ChannelComponent],
+  imports: [ChannelComponent, MainMenuComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
