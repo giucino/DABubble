fa2bdb7d38dfc14bffc8dc3fe1ee6eb20ab97fa1
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-04-19 22:33:25 +0200

    avatar html almost done

--- src/app/login-page/sign-in/sign-in.component.ts
+++ src/app/login-page/sign-in/sign-in.component.ts
@@ -1,23 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { Router, RouterModule } from '@angular/router';
 import { LoginPageComponent } from '../login-page.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [LoginComponent, RouterModule, LoginPageComponent, CommonModule, FormsModule],
+  imports: [LoginComponent, RouterModule, LoginPageComponent, CommonModule, FormsModule, MatCheckboxModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
-  
-  constructor(private router: Router) { }
+  checked = false;
+  disabled = false;
+  constructor(private router: Router, private renderer: Renderer2, private el: ElementRef) { }
 
   goToLogin() {
     this.router.navigate(['/login-page/login']);
-   
+  }
+
+  goToAvatar() {
+    this.router.navigate(['/login-page/avatar']);
+  }
+
+  ngAfterViewInit() {
+      const aElements = this.el.nativeElement.querySelectorAll('a');
+  
+      aElements.forEach((aElement: { parentElement: { previousElementSibling: any; }; }) => {
+          this.renderer.listen(aElement, 'mouseover', () => {
+              const inputElement = aElement.parentElement.previousElementSibling;
+              if (inputElement && inputElement.type === 'checkbox') {
+                  this.renderer.setStyle(inputElement, 'background-color', 'var(--color-bg)');
+              }
+          });
+  
+          this.renderer.listen(aElement, 'mouseout', () => {
+              const inputElement = aElement.parentElement.previousElementSibling;
+              if (inputElement && inputElement.type === 'checkbox') {
+                  this.renderer.setStyle(inputElement, 'background-color', 'white');
+              }
+          });
+      });
   }
 }
+
+
+
