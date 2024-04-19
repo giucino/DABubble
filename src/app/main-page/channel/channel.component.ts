4b1a6f4b840f9c75f577e115456f9c214915cb12
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-04-19 13:24:54 +0200

    added channel footer, message input

--- src/app/main-page/channel/channel.component.ts
+++ src/app/main-page/channel/channel.component.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-channel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss'
 })
 export class ChannelComponent {
 
-  channelType : 'main' | 'direct' | 'thread' | 'new' = 'new';
+  channelType : 'main' | 'direct' | 'thread' | 'new' = 'main';
 }
