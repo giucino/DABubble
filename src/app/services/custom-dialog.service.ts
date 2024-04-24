5494173abc053f2cc002104bf8b0aa4ce917d32b
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-04-24 02:16:19 +0200

    added basic dialog service

---
+++ src/app/services/custom-dialog.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ComponentType } from '@angular/cdk/portal';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomDialogService {
+  dialogRef: MatDialogRef<any> | null = null;
+
+  constructor(public dialog: MatDialog) {
+  }
+
+  public openDialog(button: HTMLElement,dialogComponent : ComponentType<any>) {
+    const rect = button.getBoundingClientRect();
+    this.dialogRef = this.dialog.open(dialogComponent, {
+      panelClass: 'custom-dialog-anchorTopRight',
+      position: {
+        top: rect.bottom + 'px',
+        left: rect.right + 'px',
+      },
+    });
+    if (this.dialogRef) {
+      window.addEventListener('resize', () =>
+        this.updateDialogPosition(button)
+      );
+    }
+  }
+
+  // Funktion zum Aktualisieren der Dialogposition
+  updateDialogPosition(button: HTMLElement): void {
+    if (this.dialogRef) {
+      const rect = button.getBoundingClientRect();
+      this.dialogRef.updatePosition({
+        top: rect.bottom + 'px',
+        left: rect.right + 'px',
+      });
+    }
+  }
+}
