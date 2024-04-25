5bf5ce023b32a9384ff7335de58ebed864a79ce2
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-04-25 09:59:43 +0200

    update interfaces

--- src/app/interfaces/channel.interface.ts
+++ src/app/interfaces/channel.interface.ts
@@ -1,3 +1,13 @@
 export interface Channel {
-    
+    id?: string,
+    name : string,
+    description : string,
+    created_at: number,
+    creator: {
+        user_id: string,
+        name: string,
+    },
+    members: string[],
+    active_members: string[],
+    channel_type: 'main' | 'direct' | 'thread' ,
 }
--- src/app/interfaces/message.interface.ts
+++ src/app/interfaces/message.interface.ts
@@ -1,3 +1,16 @@
 export interface Message {
-    
+    id?: string,
+    user_id: string,
+    channel_id: string,
+    thread_id?: string,
+    message: {
+        text: string, // 'This is an example <@user_id> <#channel_id>'
+        user_name: string,
+        reactions?: string[], // 'reaction_id_1', 'reaction_id_2' ...
+        attachements?: string[], // 'img.jpg' , 'document.pdf' ...
+    }
+    created_at: number,
+    modified_at?: number,
+    is_deleted: boolean,
+    total_replies?: number,
 }
