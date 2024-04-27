dbf558d2797cc54319f41bc309a5f2b9bfbfd5ca
giucino <125276491+giucino@users.noreply.github.com>
2024-04-27 16:58:15 +0200

    Update ChannelService and Channel interface

--- src/app/interfaces/channel.interface.ts
+++ src/app/interfaces/channel.interface.ts
@@ -1,10 +1,12 @@
+import { ChannelTypeEnum } from '../shared/enums/channel-type.enum';
+
 export interface Channel {
-    id?: string,
-    name : string,
-    description : string,
-    created_at: number,
-    creator: string, // 'user_id'
-    members: string[], 
-    active_members: string[],
-    channel_type: 'main' | 'direct' | 'thread' ,
+  id?: string;
+  name: string;
+  description: string;
+  created_at: number;
+  creator: string; // 'user_id'
+  members: string[];
+  active_members: string[];
+  channel_type: ChannelTypeEnum;
 }
