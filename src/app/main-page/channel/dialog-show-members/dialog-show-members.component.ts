7034515bad3128102412ba5fabf4203b4532476f
giucino <125276491+giucino@users.noreply.github.com>
2024-05-06 20:33:45 +0200

    Update dialog-show-members component to dynamically display user information

--- src/app/main-page/channel/dialog-show-members/dialog-show-members.component.ts
+++ src/app/main-page/channel/dialog-show-members/dialog-show-members.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../firebase.service/user.service';
+import { User } from '../../../interfaces/user.interface';
 
 @Component({
   selector: 'app-dialog-show-members',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, FormsModule],
   templateUrl: './dialog-show-members.component.html',
-  styleUrl: './dialog-show-members.component.scss'
+  styleUrl: './dialog-show-members.component.scss',
 })
 export class DialogShowMembersComponent {
-
-  constructor(public dialogRef : MatDialogRef<DialogShowMembersComponent>) {
-    
-  }
-
+  constructor(
+    public dialogRef: MatDialogRef<DialogShowMembersComponent>,
+    public userService: UserService
+  ) {}
 }
