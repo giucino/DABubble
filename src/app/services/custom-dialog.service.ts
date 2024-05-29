3834f4649654ce6038110ec40dbfc295ba387865
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-05-29 14:03:48 +0200

    added mobile to custom-dialog

--- src/app/services/custom-dialog.service.ts
+++ src/app/services/custom-dialog.service.ts
@@ -8,36 +8,36 @@ import { ComponentType } from '@angular/cdk/portal';
 export class CustomDialogService {
   dialogRef: MatDialogRef<any> | null = null;
 
-  isMobile()  { return window.innerWidth <= 768;}
-
-  constructor(public dialog: MatDialog) {
+  isMobile() {
+    return window.innerWidth <= 768;
   }
 
-  public openDialog(dialogComponent : ComponentType<any>) : MatDialogRef<any> {
+  constructor(public dialog: MatDialog) {}
+
+  public openDialog(dialogComponent: ComponentType<any>): MatDialogRef<any> {
     return this.dialog.open(dialogComponent, {
       panelClass: 'custom-dialog',
     });
   }
 
-  public openDialogAbsolute(button: HTMLElement,dialogComponent : ComponentType<any>, position: 'left' | 'right', mobilePosition? : 'mid' | 'bottom') {
+  public openDialogAbsolute(
+    button: HTMLElement,
+    dialogComponent: ComponentType<any>,
+    position: 'left' | 'right',
+    mobilePosition?: 'mid' | 'bottom'
+  ) {
     // const rect = button.getBoundingClientRect();
     // let isMobile = window.innerWidth <= 768;
     let positionAsJSON = this.getPosition(button, position, mobilePosition);
-
+    let panelClass = this.getPanelClass(position, mobilePosition);
 
     // let mPosition = mobilePosition == 'bottom' ? {bottom: '0px', left: '0px'} : {};
-    if (position == 'left') {
-      this.dialogRef = this.dialog.open(dialogComponent, {
-        panelClass: 'custom-dialog-anchorTopLeft',
-        position: positionAsJSON,
-      });
-    }
-    if (position == 'right') {
-      this.dialogRef = this.dialog.open(dialogComponent, {
-        panelClass: 'custom-dialog-anchorTopRight',
-        position: positionAsJSON,
-      });
-    }
+
+    this.dialogRef = this.dialog.open(dialogComponent, {
+      panelClass: panelClass,
+      position: positionAsJSON,
+    });
+
     if (this.dialogRef) {
       window.addEventListener('resize', () =>
         this.updateDialogPosition(button, position, mobilePosition)
@@ -46,7 +46,11 @@ export class CustomDialogService {
   }
 
   // Funktion zum Aktualisieren der Dialogposition
-  updateDialogPosition(button: HTMLElement, position: 'left' | 'right', mobilePosition? : 'mid' | 'bottom'): void {
+  updateDialogPosition(
+    button: HTMLElement,
+    position: 'left' | 'right',
+    mobilePosition?: 'mid' | 'bottom'
+  ): void {
     if (this.dialogRef) {
       // const rect = button.getBoundingClientRect();
       // this.dialogRef.updatePosition({
@@ -59,26 +63,38 @@ export class CustomDialogService {
     }
   }
 
-
-  getPosition(button: HTMLElement,position : 'left' | 'right', mobilePosition? : 'mid' | 'bottom') {
-    if(this.isMobile() && mobilePosition) {
+  getPosition(
+    button: HTMLElement,
+    position: 'left' | 'right',
+    mobilePosition?: 'mid' | 'bottom'
+  ) {
+    if (this.isMobile() && mobilePosition) {
       return this.getMobilePosition(mobilePosition);
     } else {
       const rect = button.getBoundingClientRect();
       return {
         top: rect.bottom + 'px',
-        left: position == 'left' ? rect.left +'px' : rect.right + 'px' ,
-      }
+        left: position == 'left' ? rect.left + 'px' : rect.right + 'px',
+      };
     }
   }
 
-  getMobilePosition(mobilePosition : 'mid' | 'bottom') {
+  getMobilePosition(mobilePosition: 'mid' | 'bottom') {
     switch (mobilePosition) {
       case 'mid':
         return {};
-      case 'bottom': 
-        return {left: '0px', bottom: '0px'};
+      case 'bottom':
+        return { left: '0px', bottom: '0px' };
     }
   }
 
+  getPanelClass(position: 'left' | 'right', mobilePosition?: 'mid' | 'bottom') {
+    let pannelClass = '';
+    pannelClass =
+      position == 'left'
+        ? 'custom-dialog-anchorTopLeft'
+        : 'custom-dialog-anchorTopRight';
+    if (mobilePosition) pannelClass += '-mobile';
+    return pannelClass;
+  }
 }
